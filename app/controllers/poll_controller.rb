64978e563ff42b042fd83466d44fa95d5bced1b0
neebz <muneeb83@gmail.com>
2012-05-20 17:18:54 +0500

    return constituencies and parties from index

--- app/controllers/poll_controller.rb
+++ app/controllers/poll_controller.rb
@@ -2,7 +2,8 @@ class PollController < ApplicationController
 	respond_to :json, except: [:index]
 	
 	def index
-		
+		@constituencies = Constituency.find :all
+		@parties = Party.find :all
 	end
 
 	def results
